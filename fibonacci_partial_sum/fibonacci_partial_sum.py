553f7b3bedb46263c5f290efb617d9ac1fea4af9
Robert Tuck <robtuck@gmail.com>
2017-11-16 11:04:31 -0500

    week 1 problems #5 - #7

--- fibonacci_partial_sum/fibonacci_partial_sum.py
+++ fibonacci_partial_sum/fibonacci_partial_sum.py
@@ -1,11 +1,12 @@
 # Uses python3
 import sys
 
+
 def fibonacci_partial_sum_naive(from_, to):
     sum = 0
 
     current = 0
-    next  = 1
+    next = 1
 
     for i in range(to + 1):
         if i >= from_:
@@ -16,7 +17,66 @@ def fibonacci_partial_sum_naive(from_, to):
     return sum % 10
 
 
+def pisano(m):
+    a = 0
+    b = 1
+
+    for i in range(0, m * m):
+        a, b = b, (a + b) % m
+        if a == 0 and b == 1:
+            return i + 1
+
+
+def calc_fib_better(n):
+    # if (n <= 1):
+        # return n
+
+    f = []
+    a = 0
+    b = 1
+
+    for i in range(n + 1):
+        f.append(a)
+        a, b = b, a + b
+
+    return f[n]
+
+
+def get_fibonacci_huge_better(n, m):
+    if n <= 1:
+        return n
+
+    p = pisano(m)
+
+    f_n_remainder = n % p
+
+    last_digit = calc_fib_better(f_n_remainder) % m
+
+    return last_digit
+
+
+def fibonacci_sum_better(n):
+    test = get_fibonacci_huge_better(n + 2, 10) - 1
+
+    # print("FSB: " + str(n) + " " + str(test))
+
+    return test
+
+
+def fibonacci_partial_sum_better(from_, to):
+    if from_ == to:
+        return get_fibonacci_huge_better(from_, 10)
+
+    result = fibonacci_sum_better(to) - fibonacci_sum_better(from_ - 1)
+
+    if result < 0:
+        return result + 10
+
+    return result
+
+
 if __name__ == '__main__':
-    input = sys.stdin.read();
+    input = sys.stdin.readline()
     from_, to = map(int, input.split())
-    print(fibonacci_partial_sum_naive(from_, to))
+    # print(fibonacci_partial_sum_naive(from_, to))
+    print(fibonacci_partial_sum_better(from_, to))
--- fibonacci_sum_last_digit/fibonacci_sum_last_digit.py
+++ fibonacci_sum_last_digit/fibonacci_sum_last_digit.py
@@ -1,13 +1,14 @@
 # Uses python3
 import sys
 
+
 def fibonacci_sum_naive(n):
     if n <= 1:
         return n
 
     previous = 0
-    current  = 1
-    sum      = 1
+    current = 1
+    sum = 1
 
     for _ in range(n - 1):
         previous, current = current, previous + current
@@ -15,7 +16,55 @@ def fibonacci_sum_naive(n):
 
     return sum % 10
 
+
+def pisano(m):
+    a = 0
+    b = 1
+
+    for i in range(0, m * m):
+        a, b = b, (a + b) % m
+        if a == 0 and b == 1:
+            return i + 1
+
+
+def calc_fib_better(n):
+    # if (n <= 1):
+        # return n
+
+    f = []
+    a = 0
+    b = 1
+
+    for i in range(n + 1):
+        f.append(a)
+        a, b = b, a + b
+
+    return f[n]
+
+
+def get_fibonacci_huge_better(n, m):
+    if n <= 1:
+        return n
+
+    p = pisano(m)
+
+    f_n_remainder = n % p
+
+    last_digit = calc_fib_better(f_n_remainder) % m
+
+    if last_digit == 0:
+        return 10
+
+    return last_digit
+
+
+def fibonacci_sum_better(n):
+
+    return get_fibonacci_huge_better(n + 2, 10) - 1
+
+
 if __name__ == '__main__':
-    input = sys.stdin.read()
+    input = sys.stdin.readline()
     n = int(input)
-    print(fibonacci_sum_naive(n))
+    # print(fibonacci_sum_naive(n))
+    print(fibonacci_sum_better(n))
