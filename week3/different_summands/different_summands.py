192509f2e6378ad59a3403454cc662b67c23bc00
Robert Tuck <robtuck@gmail.com>
2017-12-04 11:10:56 -0500

    added different_summands to week3

--- week3/different_summands/different_summands.py
+++ week3/different_summands/different_summands.py
@@ -1,10 +1,26 @@
 # Uses python3
 import sys
 
+
 def optimal_summands(n):
     summands = []
-    #write your code here
+    # write your code here
+    k = n
+    m = 1
+    if n == 2 or n == 1:
+        summands.append(n)
+    else:
+        for i in range(1, k):
+            if k <= 2 * m:
+                summands.append(k)
+                return summands
+            else:
+                summands.append(m)
+                k = k - m
+                m = m + 1
+
     return summands
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
